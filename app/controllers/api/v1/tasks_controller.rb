176635c29c19ba5630d31dd1c2467b9baeff26fe
brianryu <s.brianryu@gmail.com>
2019-05-06 15:58:26 -0400

    Tasks + Trips controllers Complete

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,35 @@
 class Api::V1::TasksController < ApplicationController
+    before_action :find_task, only: [:update]
+    def index
+        @tasks = Task.all
+        render json: @tasks
+    end
+
+    def new
+        @task = Task.new
+    end 
+
+    def create
+        @task = Task.create(task_params)
+        render json: @task, status: :accepted
+    end 
+    
+    def update
+        @Task.update(task_params)
+        if @task.save
+        render json: @task, status: :accepted
+        else
+        render json: { errors: @task.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
+    
+    private
+    
+    def task_params
+        params.permit(:todo_id, :message)
+    end
+    
+    def find_task
+        @task = Task.find(params[:id])
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,22 +12,22 @@ Task.destroy_all
 Todo.destroy_all
 
 # Users
-brian = User.create(username: "brian", password: "hi")
-meredith = User.create(username: "meredith", password: "hi")
+brian = User.create!(username: "brian", password: "hi")
+meredith = User.create!(username: "meredith", password: "hi")
 
 # Trips
-california = Trip.create(name: "cali", img_url: "http://www.californiabeaches.com/wp-content/uploads/2015/04/bigstock-Surf-City-Huntington-Beach-Pie-24727454.jpg")
-nyc = Trip.create(name: "nyc", img_url: "https://www.nycgo.com/images/pages/347/river-skyline-views-brooklyn-nyc-photo-julienne-schaer-010__large.jpg")
+california = Trip.create!(name: "cali", img_url: "http://www.californiabeaches.com/wp-content/uploads/2015/04/bigstock-Surf-City-Huntington-Beach-Pie-24727454.jpg")
+nyc = Trip.create!(name: "nyc", img_url: "https://www.nycgo.com/images/pages/347/river-skyline-views-brooklyn-nyc-photo-julienne-schaer-010__large.jpg")
 
 # Todos
-todoBrian = Todo.create(user_id: brian.id, trip_id: california.id, location: "Orange County, CA", start_date: "2019-7-2", end_date: "2019-7-17", priority_lvl: 5)
-todoMere = Todo.create(user_id: meredith.id, trip_id: nyc.id, location: "New York, NY", start_date: "2019-10-15", end_date: "2019-10-25", priority_lvl: 4)
+todoBrian = Todo.create!(user_id: brian.id, trip_id: california.id, location: "Orange County, CA", start_date: "2019-7-2", end_date: "2019-7-17", priority_lvl: 5)
+todoMere = Todo.create!(user_id: meredith.id, trip_id: nyc.id, location: "New York, NY", start_date: "2019-10-15", end_date: "2019-10-25", priority_lvl: 4)
 
 # Tasks
-cali1 = Task.create(todo_id: todoBrian.id, message: "Go Get Some Tacos!")
-cali2 = Task.create(todo_id: todoBrian.id, message: "Universal Studio")
-cali3 = Task.create(todo_id: todoBrian.id, message: "Movement Lifestyle Dance Studio")
+cali1 = Task.create!(todo_id: todoBrian.id, message: "Go Get Some Tacos!")
+cali2 = Task.create!(todo_id: todoBrian.id, message: "Universal Studio")
+cali3 = Task.create!(todo_id: todoBrian.id, message: "Movement Lifestyle Dance Studio")
 
-nyc1 = Task.create(todo_id: todoMere.id, message: "Go Get Some PIZZA!")
-nyc2 = Task.create(todo_id: todoMere.id, message: "Statue Of Liberty")
-nyc3 = Task.create(todo_id: todoMere.id, message: "Times Square Visit")
+nyc1 = Task.create!(todo_id: todoMere.id, message: "Go Get Some PIZZA!")
+nyc2 = Task.create!(todo_id: todoMere.id, message: "Statue Of Liberty")
+nyc3 = Task.create!(todo_id: todoMere.id, message: "Times Square Visit")
