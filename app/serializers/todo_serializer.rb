374e34776edeb900797f9eb4805e52be8b8a37e3
brianryu <s.brianryu@gmail.com>
2019-05-07 09:22:33 -0400

    Modified all serializer files with appropriate associations

--- app/serializers/todo_serializer.rb
+++ app/serializers/todo_serializer.rb
@@ -1,3 +1,6 @@
 class TodoSerializer < ActiveModel::Serializer
   attributes :id, :location, :start_date, :end_date, :priority_lvl
+  has_many :tasks
+  belongs_to :user
+  belongs_to :trip
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username 
+  has_many :todo
+  has_many :trips
 end
